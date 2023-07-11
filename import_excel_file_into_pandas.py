08a0fd818045cd2efc8b6544231d6e8c9944d59c
Paul Hicks <126404389+paulhicks01@users.noreply.github.com>
2023-07-11 12:53:25 +0100

    Python code added

---
+++ import_excel_file_into_pandas.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+# import pandas package as pd
+import pandas as pd
+
+# read excel file via url, specifying sheet name
+df = pd.read_excel('https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1164031/disability-confident-list-of-employers.xlsx',sheet_name= 'DC Sign ups')
+
+# filter Town/City field
+df[df.Town_City=="Oxford"]
+
